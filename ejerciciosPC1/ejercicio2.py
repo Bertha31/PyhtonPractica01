ada26e8efa2b4dcb89a518c02cf01669c2f21fd7
Bertha31 <145274441+Bertha31@users.noreply.github.com>
2023-09-19 01:07:05 +0000

    dede

--- ejerciciosPC1/ejercicio2.py
+++ ejerciciosPC1/ejercicio2.py
@@ -1,4 +1,6 @@
-consumo= int(input('¿Cuánto fue su consumo?: S/.'))
-propina= int(input('¿Qué porcentaje de propina desea dejar al mesero?: '))
+consumo= float(input('¿Cuánto fue su consumo?: S/.'))
+propina= float(input('¿Qué porcentaje de propina desea dejar al mesero? (ingresar numero sin %): '))
 total= consumo + consumo*propina/100
-print(f'El monto total a pagar es: S/. {total}')
+totalpropina= consumo*propina/100
+print(f'Propina en total: S/. {totalpropina}')
+print(f'El monto total a pagar (Consumo + Propina) es: S/. {total:.2f}')
