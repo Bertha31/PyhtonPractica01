22d23cb1140face665a71b03d34093c4fe6963fd
Bertha31 <145274441+Bertha31@users.noreply.github.com>
2023-09-20 01:56:57 +0000

    jjj

---
+++ ejerciciosPC1/ejercicio8.py
@@ -0,0 +1,15 @@
+hora_usuario = input("Por favor, ingrese la hora en formato (HH:MM): ")
+try:
+    horas, minutos = map(int, hora_usuario.split(":"))
+except ValueError:
+    print("Formato de hora incorrecto. Use HH:MM.")
+    exit()
+
+if 7 <= horas < 8 or (horas == 8 and minutos == 0):
+    print("Es hora de desayunar.")
+elif 12 <= horas < 13 or (horas == 13 and minutos == 0):
+    print("Es hora de almorzar.")
+elif 18 <= horas < 19 or (horas == 19 and minutos == 0):
+    print("Es hora de cenar.")
+else:
+    print("No es hora de comer.")
